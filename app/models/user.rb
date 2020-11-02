fe6d076bf9f47dbd5244f6cd2bc8e1350eac74b7
guillainbisimwa <guillainbisimwa@hotmail.fr>
2020-11-02 18:33:30 +0200

    User Model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
-  validates :username, presence: true #, uniqueness: true #, length: { in: 4..20 }
-  validates :email, presence: true # , uniqueness: true#, length: { in: 10..30 }
-  validates :password, presence: true #, length: { in: 8..30 }
+  validates :username, presence: true, uniqueness: true, length: { in: 4..20 }
+  validates :email, presence: true, uniqueness: true, length: { in: 10..30 }
+  validates :password, presence: true, length: { in: 8..30 }
   # has_many posts
 end
