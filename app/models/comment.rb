2d1ba714bdd6d8d48c270a249c37c198e67976b9
alvaro <aaruizf@unal.edu.co>
2020-11-02 13:01:30 -0500

    Add comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ApplicationRecord
+  validates :body, presence: true, length: { minimum: 5 }
+  belongs_to :user
+  belongs_to :post
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, length: { in: 10..30 }
   validates :password, presence: true, length: { in: 8..30 }
   has_many :posts
+  has_many :comments, through: :posts
 end
