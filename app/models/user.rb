d5ece5d042dbe3d8dc78bb5e9fe407c2371bbb33
alvaro <aaruizf@unal.edu.co>
2020-11-02 13:06:54 -0500

    Fix some Rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-
   validates :username, presence: true, uniqueness: true, length: { in: 4..20 }
   validates :email, presence: true, uniqueness: true, length: { in: 10..30 }
   validates :password, presence: true, length: { in: 8..30 }
