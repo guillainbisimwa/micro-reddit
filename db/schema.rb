949c9f4817b18b25812b5a1e4de70eab8678edc5
alvaro <aaruizf@unal.edu.co>
2020-11-02 11:23:43 -0500

    Add validations for User model

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_11_02_152401) do
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name"
+    t.string "username"
     t.string "email"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
